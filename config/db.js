33b5658182bbc57d7cce553ac6ee3ac9105c5e1e
Satvik Vadigi <satvikjee12@gmail.com>
2026-03-19 18:20:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 
 const connectDB = async() => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL_LOCAL);
+        const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log(`Connected to MongoDB Database: ${mongoose.connection.host}`.bgGreen.white);
     } catch (error) {
         console.log(`MongoDB connection error: ${error.message}`.bgRed.white);
