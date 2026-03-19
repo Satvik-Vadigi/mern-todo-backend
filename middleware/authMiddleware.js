af1b2e112d9da7d5272393c71163962f8a79b9e6
Satvik Vadigi <satvikjee12@gmail.com>
2026-03-19 20:01:53 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,9 +2,18 @@ const JWT = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
-    //get token bearer
-    const token = req.headers["authorization"].split(" ")[1];
-    JWT.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader) {
+      return res.status(401).send({
+        success: false,
+        message: "No token provided",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    JWT.verify(token, process.env.jwt_secret, (err, decoded) => {
       if (err) {
         return res.status(401).send({
           success: false,
@@ -19,7 +28,7 @@ module.exports = (req, res, next) => {
     console.log(error);
     res.status(400).send({
       success: false,
-      message: "Please provide Auth token",
+      message: "Token error",
       error,
     });
   }
